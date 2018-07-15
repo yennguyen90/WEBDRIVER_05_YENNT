83a7f57cb8a1859167c388807782872cb594b072
Yennt <yennt268@gmail.com>
2018-07-15 11:23:36 +0700

    Add first commit

---
+++ src/selenium_api/Topic_01_CheckEnviroment.java
@@ -0,0 +1,39 @@
+package selenium_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_CheckEnviroment {
+	WebDriver driver;
+
+	@Test
+	public void TC_01_CheckUrlAndTitle() {
+		System.out.println("Check homepage title");
+		String homePageTitle = driver.getTitle();
+		Assert.assertEquals(homePageTitle, "Guru99 Bank Home Page");
+
+		System.out.println("Check homepage url");
+		String homePageUrl = driver.getCurrentUrl();
+		Assert.assertEquals(homePageUrl, "http://demo.guru99.com/v4/");
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.get("http://demo.guru99.com/v4/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
